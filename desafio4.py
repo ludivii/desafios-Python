aac836931f0beb60b5c5b08506f53a72f77f3485
Luan <luandsvt@gmail.com>
2024-11-25 20:54:32 -0300

    desafio 4

---
+++ desafio4.py
@@ -0,0 +1,6 @@
+name = input("Digite seu nome: ")
+age = int(input("Digite sua idade: "))
+
+print("Olá, {}! Você tem {} anos.".format(name,age))
+print("Olá,", name,"! Você tem", age, "anos.")
+print(f"Olá, {name}! Você tem {age} anos.")
