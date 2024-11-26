f806a4f5dd974221766ec52b31224581158ada3d
Luan <luandsvt@gmail.com>
2024-11-25 21:07:25 -0300

    desafio 5

--- desafio4.py
+++ desafio4.py
@@ -1,6 +1,6 @@
 name = input("Digite seu nome: ")
 age = int(input("Digite sua idade: "))
 
+print("Olá,", name, "! Você tem", age, "anos.")
 print("Olá, {}! Você tem {} anos.".format(name,age))
-print("Olá,", name,"! Você tem", age, "anos.")
 print(f"Olá, {name}! Você tem {age} anos.")
---
+++ desafio5.py
@@ -0,0 +1,8 @@
+num1 = float(input("Digite o 1º número: "))
+num2 = float(input("Digite o 2º número: "))
+
+print(f"Soma = {num1 + num2}")
+print(f"Subtração = {num1 - num2}")
+print(f"Multiplicação = {num1 * num2}")
+print(f"Divisão = {num1 / num2}")
+print(f"Exponenciação = {num1 ** num2}")
