06fcbb2c5db0d97b483a9e58013a6dd4fad312d3
Luan <luandsvt@gmail.com>
2024-11-27 07:50:45 -0300

    Desafio 12

---
+++ desafio12.py
@@ -0,0 +1,10 @@
+## Desafio 12
+# Crie uma list de frutas e outra de vegetais, use um for aninhado para
+# dar print com todas as combinações
+
+frutas = ["Maçã", "Uva", "Banana"]
+vegetais = ["Cenoura", "Repolho", "Batata"]
+
+for fruta in frutas:
+    for vegetal in vegetais:
+        print(fruta, vegetal)
