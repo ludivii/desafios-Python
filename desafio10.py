02c01cbb42a0dd15868cc8e5f1b64449d9285d63
Luan <luandsvt@gmail.com>
2024-11-27 07:34:30 -0300

    Desafio 10

---
+++ desafio10.py
@@ -0,0 +1,7 @@
+## Desafio 10
+# Use a nova list frutas, use for loop e print cada fruta separadamente
+
+frutas = ['maçã', 'morango', 'uva']
+
+for fruta in frutas:
+    print(f"Eu gosto de {fruta}")
