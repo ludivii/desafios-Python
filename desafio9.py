f61ed007cc91778c92dfb821bf1d976bfca3b8fc
Luan <luandsvt@gmail.com>
2024-11-27 07:27:08 -0300

    Desafio 9

---
+++ desafio9.py
@@ -0,0 +1,11 @@
+## Desafio 9
+# Use a nova list frutas, remove() manga, del -1 e print
+
+frutas = ['maçã', 'morango', 'manga', 'uva', 'abacaxi']
+
+print(frutas)
+
+frutas.remove("manga")
+del frutas[-1]
+
+print(frutas)
