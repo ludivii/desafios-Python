2a0f4b930fbddf98918c65f51cd23b824ac2957a
Luan <luandsvt@gmail.com>
2024-11-27 07:20:10 -0300

    Desafio 8

---
+++ desafio8.py
@@ -0,0 +1,14 @@
+## Desafio 8
+# Use a list frutas, altere o index 1, add mais uma fruta e print
+
+frutas = ["maçã", "banana", "manga", "uva"]
+
+print(frutas)
+
+frutas[1] = "morango"
+
+print(frutas)
+
+frutas.append("abacaxi")
+
+print(frutas)
